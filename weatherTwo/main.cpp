f88ef5e22c55de99a8ea78a0c5cc99761b70c275
webBoy96 <habrin777@yandex.ru>
2024-02-23 13:33:59 +0500

    Background fixed

--- weatherTwo/main.cpp
+++ weatherTwo/main.cpp
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
     QApplication::setQuitOnLastWindowClosed(false);
     //QWidget background(nullptr, Qt::FramelessWindowHint);
     //background.setAttribute(Qt::WA_TranslucentBackground);
-    Widget w(nullptr, Qt::FramelessWindowHint | Qt::Popup);
+    Widget w(nullptr, Qt::FramelessWindowHint);
     w.setAttribute(Qt::WA_NoSystemBackground);
     w.setAttribute(Qt::WA_TranslucentBackground);
     w.setAttribute(Qt::WA_PaintOnScreen);
